37189bb88d8440af013335def7889b2db45f5f59
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 15:42:39 +0100

    commit

--- src/main/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplication.java
+++ src/main/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplication.java
@@ -1,13 +1,19 @@
 package dev.elma.orm_jpa_association;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrmJpaAssociationApplication {
+public class OrmJpaAssociationApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+
         SpringApplication.run(OrmJpaAssociationApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
