53e03c95465838a6bec2314f097b7ee93670f359
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 15:56:44 +0100

    commit

--- src/main/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplication.java
+++ src/main/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplication.java
@@ -1,12 +1,20 @@
 package dev.elma.orm_jpa_association;
 
+import dev.elma.orm_jpa_association.entities.Patient;
+import dev.elma.orm_jpa_association.repositories.PatientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+import java.util.stream.Stream;
+
 @SpringBootApplication
+@AllArgsConstructor
 public class OrmJpaAssociationApplication implements CommandLineRunner {
 
+    PatientRepository patientRepository;
     public static void main(String[] args) {
 
         SpringApplication.run(OrmJpaAssociationApplication.class, args);
@@ -15,5 +23,14 @@ public class OrmJpaAssociationApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Stream.of("Ahmed","Ibrahim","Nadia")
+                .forEach(name->{
+                    Patient p=Patient.builder().
+                            birthDay(new Date()).
+                            name("Abdeljalil").
+                            sick(true).
+                            build();
+                    patientRepository.save(p);
+                });
     }
 }
