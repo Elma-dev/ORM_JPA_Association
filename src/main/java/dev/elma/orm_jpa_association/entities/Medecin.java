ea25d23e8d60601bf7b5eae063c933ed89d3f2a1
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 15:28:50 +0100

    commit

--- src/main/java/dev/elma/orm_jpa_association/entities/Medecin.java
+++ src/main/java/dev/elma/orm_jpa_association/entities/Medecin.java
@@ -1,11 +1,21 @@
 package dev.elma.orm_jpa_association.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Collection;
 
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Medecin {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String speciality;
     private String email;
+    @OneToMany
     private Collection<RendezVous> rendezVousCollection;
 }
