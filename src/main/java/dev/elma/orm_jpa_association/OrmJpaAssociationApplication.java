3f8c92b29af74bb984dd4a670ed9fab5603b0229
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-25 15:15:44 +0100

    Initial commit

---
+++ src/main/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.orm_jpa_association;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmJpaAssociationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmJpaAssociationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/orm_jpa_association/OrmJpaAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.orm_jpa_association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaAssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
